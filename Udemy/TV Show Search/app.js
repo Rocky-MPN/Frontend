01977d85655140e0adc37327fc8074b44b2b77f3
Rocky <78936592+Rocky-MPN@users.noreply.github.com>
2023-01-24 13:51:54 +0000

    Update app.js

--- Udemy/TV Show Search/app.js
+++ Udemy/TV Show Search/app.js
@@ -2,6 +2,18 @@ const form = document.querySelector('#searchForm');
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     const searchTerm = form.elements.query.value;
-    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    console.log(res.data[0].show.image.medium);
+    const config = { params: { q: searchTerm } }
+    const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data)
+    form.elements.query.value = ';'
 })
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+}
