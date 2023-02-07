afe6b71f687ea324fee75bdaeac7a2b51415a7ac
Rocky <78936592+Rocky-MPN@users.noreply.github.com>
2023-02-07 13:47:33 +0000

    frogger

--- FreeCodeCamp/Frogger-Game/app.js
+++ FreeCodeCamp/Frogger-Game/app.js
@@ -4,7 +4,12 @@ const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
 let currentIndex = 76
 const width = 9
+let timerId
+
 const logsLeft = document.querySelectorAll('.log-left')
+const logsRight = document.querySelectorAll('.log-right')
+const carsLeft = document.querySelectorAll('.car-left')
+const carsRight = document.querySelectorAll('.car-right')
 
 function moveFrog(e) {
     squares[currentIndex].classList.remove('frog')
@@ -38,34 +43,104 @@ function moveFrog(e) {
 document.addEventListener('keyup', moveFrog)
 
 
-function autoMoveLogs() {
+function autoMoveElements() {
     logsLeft.forEach(logLeft => moveLogLeft(logLeft))
+    logsRight.forEach(logRight => moveLogRight(logRight))
+    carsLeft.forEach(carLeft => moveCarLeft(carLeft))
+    carsRight.forEach(carRight => moveCarRight(carRight))
 }
 
 
 function moveLogLeft(logLeft) {
     switch (true) {
-        case logLeft.classList.contains('.l1'):
-            logLeft.classList.remove('.l1')
-            logLeft.classList.add('.l2')
+        case logLeft.classList.contains('l1'):
+            logLeft.classList.remove('l1')
+            logLeft.classList.add('l2')
             break
-        case logLeft.classList.contains('.l2'):
-            logLeft.classList.remove('.l2')
-            logLeft.classList.add('.l3')
+        case logLeft.classList.contains('l2'):
+            logLeft.classList.remove('l2')
+            logLeft.classList.add('l3')
             break
-        case logLeft.classList.contains('.l3'):
-            logLeft.classList.remove('.l3')
-            logLeft.classList.add('.l4')
+        case logLeft.classList.contains('l3'):
+            logLeft.classList.remove('l3')
+            logLeft.classList.add('l4')
             break
-        case logLeft.classList.contains('.l4'):
-            logLeft.classList.remove('.l4')
-            logLeft.classList.add('.l5')
+        case logLeft.classList.contains('l4'):
+            logLeft.classList.remove('l4')
+            logLeft.classList.add('l5')
             break
-        case logLeft.classList.contains('.l5'):
-            logLeft.classList.remove('.l5')
-            logLeft.classList.add('.l1')
+        case logLeft.classList.contains('l5'):
+            logLeft.classList.remove('l5')
+            logLeft.classList.add('l1')
             break
     }
 }
 
-setInterval(autoMoveLogs, 1000)
+function moveLogRight(logRight) {
+    switch (true) {
+        case logRight.classList.contains('l1'):
+            logRight.classList.remove('l1')
+            logRight.classList.add('l5')
+            break
+        case logRight.classList.contains('l2'):
+            logRight.classList.remove('l2')
+            logRight.classList.add('l1')
+            break
+        case logRight.classList.contains('l3'):
+            logRight.classList.remove('l3')
+            logRight.classList.add('l2')
+            break
+        case logRight.classList.contains('l4'):
+            logRight.classList.remove('l4')
+            logRight.classList.add('l3')
+            break
+        case logRight.classList.contains('l5'):
+            logRight.classList.remove('l5')
+            logRight.classList.add('l4')
+            break
+    }
+}
+
+function moveCarLeft(carLeft) {
+    switch (true) {
+        case carLeft.classList.contains('c1'):
+            carLeft.classList.remove('c1')
+            carLeft.classList.add('c2')
+            break
+        case carLeft.classList.contains('c2'):
+            carLeft.classList.remove('c2')
+            carLeft.classList.add('c3')
+            break
+        case carLeft.classList.contains('c3'):
+            carLeft.classList.remove('c3')
+            carLeft.classList.add('c1')
+            break
+    }
+}
+
+function moveCarRight(carRight) {
+    switch (true) {
+        case carRight.classList.contains('c1'):
+            carRight.classList.remove('c1')
+            carRight.classList.add('c3')
+            break
+        case carRight.classList.contains('c2'):
+            carRight.classList.remove('c2')
+            carRight.classList.add('c1')
+            break
+        case carRight.classList.contains('c3'):
+            carRight.classList.remove('c3')
+            carRight.classList.add('c2')
+            break
+    }
+}
+
+function lose() {
+    if (squares[currentIndex].classList.contains('.c1')) {
+        resultDisplay.textContent = 'You lose!'
+        clearInterval(timerId)
+        squares[currentIndex].classList.remove('frog')
+    }
+}
+
+timerId = setInterval(autoMoveElements, 1000)
