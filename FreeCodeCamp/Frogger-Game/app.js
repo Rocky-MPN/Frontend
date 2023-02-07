de3a498ed7ed57af4a34670e19ea6cf361745ed2
Rocky <78936592+Rocky-MPN@users.noreply.github.com>
2023-02-07 16:32:39 +0000

    frogger

--- FreeCodeCamp/Frogger-Game/app.js
+++ FreeCodeCamp/Frogger-Game/app.js
@@ -1,55 +1,52 @@
+
 const timeLeftDisplay = document.querySelector('#time-left')
 const resultDisplay = document.querySelector('#result')
 const startPauseButton = document.querySelector('#start-pause-button')
 const squares = document.querySelectorAll('.grid div')
-let currentIndex = 76
-const width = 9
-let timerId
-
 const logsLeft = document.querySelectorAll('.log-left')
 const logsRight = document.querySelectorAll('.log-right')
 const carsLeft = document.querySelectorAll('.car-left')
 const carsRight = document.querySelectorAll('.car-right')
+
+let currentIndex = 76
+const width = 9
+let timerId
+let outcomeTimerId
+let currentTime = 20
 
 function moveFrog(e) {
     squares[currentIndex].classList.remove('frog')
 
     switch (e.key) {
         case 'ArrowLeft':
-            if (currentIndex % width !== 0) {
-                currentIndex -= 1
-            }
+            if (currentIndex % width !== 0) currentIndex -= 1
             break
         case 'ArrowRight':
-            if (currentIndex % width < width - 1) {
-                currentIndex += 1
-            }
+            if (currentIndex % width < width - 1) currentIndex += 1
             break
         case 'ArrowUp':
-            if (currentIndex - width >= 0) {
-                currentIndex -= width
-            }
+            if (currentIndex - width >= 0) currentIndex -= width
             break
         case 'ArrowDown':
-            if (currentIndex + width < width * width) {
-                currentIndex += width
-            }
+            if (currentIndex + width < width * width) currentIndex += width
             break
     }
     squares[currentIndex].classList.add('frog')
 }
 
-
-document.addEventListener('keyup', moveFrog)
-
-
 function autoMoveElements() {
+    currentTime--
+    timeLeftDisplay.textContent = currentTime
     logsLeft.forEach(logLeft => moveLogLeft(logLeft))
     logsRight.forEach(logRight => moveLogRight(logRight))
     carsLeft.forEach(carLeft => moveCarLeft(carLeft))
     carsRight.forEach(carRight => moveCarRight(carRight))
 }
 
+function checkOutComes() {
+    lose()
+    win()
+}
 
 function moveLogLeft(logLeft) {
     switch (true) {
@@ -136,11 +133,39 @@ function moveCarRight(carRight) {
 }
 
 function lose() {
-    if (squares[currentIndex].classList.contains('.c1')) {
+    if (
+        squares[currentIndex].classList.contains('c1') ||
+        squares[currentIndex].classList.contains('l4') ||
+        squares[currentIndex].classList.contains('l5') ||
+        currentTime <= 0
+    ) {
         resultDisplay.textContent = 'You lose!'
         clearInterval(timerId)
+        clearInterval(outcomeTimerId)
         squares[currentIndex].classList.remove('frog')
+        document.removeEventListener('keyup', moveFrog)
     }
 }
 
-timerId = setInterval(autoMoveElements, 1000)
+function win() {
+    if (squares[currentIndex].classList.contains('ending-block')) {
+        resultDisplay.textContent = 'You Win!'
+        clearInterval(timerId)
+        clearInterval(outcomeTimerId)
+        document.removeEventListener('keyup', moveFrog)
+    }
+}
+
+startPauseButton.addEventListener('click', () => {
+    if (timerId) {
+        clearInterval(timerId)
+        clearInterval(outcomeTimerId)
+        outcomeTimerId = null
+        timerId = null
+        document.removeEventListener('keyup', moveFrog)
+    } else {
+        timerId = setInterval(autoMoveElements, 1000)
+        outcomeTimerId = setInterval(checkOutComes, 50)
+        document.addEventListener('keyup', moveFrog)
+    }
+})
