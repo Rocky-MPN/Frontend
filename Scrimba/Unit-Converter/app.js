ced1b3f585424c890f76401ba624dae11a2e3d30
Rocky <78936592+Rocky-MPN@users.noreply.github.com>
2023-07-31 20:07:29 +0100

    converter

--- Scrimba/Unit-Converter/app.js
+++ Scrimba/Unit-Converter/app.js
@@ -4,4 +4,24 @@
 1 kilogram = 2.204 pound
 */
 
-let result = document.getElementById("result")
+let userInput = document.getElementById("user-input")
+let convertBtn = document.getElementById("convert-btn")
+let initialValue = document.getElementsByClassName("initial-value")
+let convertedValue = document.getElementsByClassName("converted-value")
+const meter = 3.281
+const liter = 0.264
+const kilogram = 2.204
+
+
+
+function convert() {
+    // initialValue = userInput.value
+    // console.log(initialValue)
+    console.log("clikced")
+}
+
+
+convertBtn.addEventListener("click", function () {
+    console.log("hello")
+
+})
