992a0361b6afa0037c9c189e9ec587c3dbfc1a80
Rocky <78936592+Rocky-MPN@users.noreply.github.com>
2023-08-10 05:23:49 +0100

    unit converter

--- Scrimba/Unit-Converter/app.js
+++ Scrimba/Unit-Converter/app.js
@@ -8,6 +8,7 @@ let userInput = document.getElementById("user-input")
 let convertBtn = document.getElementById("convert-btn")
 let initialValue = document.getElementsByClassName("initial-value")
 let convertedValue = document.getElementsByClassName("converted-value")
+let length = document.getElementById("length")
 const meter = 3.281
 const liter = 0.264
 const kilogram = 2.204
@@ -15,13 +16,18 @@ const kilogram = 2.204
 
 
 function convert() {
-    // initialValue = userInput.value
-    // console.log(initialValue)
-    console.log("clikced")
+    length.innerHTML = `${userInput} meters = ${userInput * meter} feet | ${userInput} feet = ${parseFloat(userInput / meter).toFixed(2)} meters`
 }
 
+userInput.addEventListener("input", function () {
+    document.getElementById("gfg")
+    .addEventListener("input", (event) => alert("Changed!"));
+})
 
 convertBtn.addEventListener("click", function () {
     console.log("hello")
-
+    console.log(userInput)
+    convert()
 })
+
+
