71cb3e76372e30b6430445fee09fcd559cf568bf
Rocky <78936592+Rocky-MPN@users.noreply.github.com>
2023-11-08 05:30:33 +0000

    restaurant app

--- Scrimba/Restaurant-Order-App/app.js
+++ Scrimba/Restaurant-Order-App/app.js
@@ -1,14 +1,34 @@
 import { menuArray } from "./data.js"
 
+let addBtn = document.getElementById('add-btn')
 
-const menuHtml = menuArray.map(function (dish) {
-    const tempArr =
+
+// render items HTML
+function getItemsHtml() {
+
+    const foodItemsArr = menuArray.map((item) => {
+        const { name, ingredients, id, price, emoji } = item
+
+        return `
+        <div class="food-item-card">
+          <p class="item-emoji">${emoji}</p>
+          <div class="item-details">
+            <h4 class="item-name">${name}</h4>
+            <p class="item-ingredients">${ingredients.join(", ")}</p>
+            <p class="item-price">$${price}</p>
+          </div>
+          <div class="btn-wrapper">
+            <button class="add-btn" data-add=${id}>+</button>
+          </div>
+        </div> 
         `
-<div id='item-1'>${dish.name}</div>
-`
+    }).join('')
+
+    return foodItemsArr
+}
 
 
-    return tempArr
-}).join('')
 
-document.querySelector('main').innerHTML = menuHtml
+
+document.querySelector('.menu-items').innerHTML = getItemsHtml()
+
